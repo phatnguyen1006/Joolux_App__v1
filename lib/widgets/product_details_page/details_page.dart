095686dea10ecc018d5aa8e4a757f6863e5e476d
unknown <19521712@gm.uit.edu.vn>
2021-07-13 21:26:34 +0700

    Khue - app pop bar

--- lib/widgets/product_details_page/details_page.dart
+++ lib/widgets/product_details_page/details_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-
+import '../Layout/app_pop_bar.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appPopBar(context),
+      body: Container(),
+    );
   }
 }
